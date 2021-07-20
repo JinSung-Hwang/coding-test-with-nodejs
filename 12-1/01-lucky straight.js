ce51f049c0a9cffe3b0a724e1d298b9c7ad546dd
JinSung-Hwang <wlstjd7782>
2021-07-20 22:53:59 +0900

    feature: 럭키 스트레이트 문제 추가

---
+++ 12-1/01-lucky straight.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+
+
+function luckyStraight(number) {
+  const numberSTR = '' + number;
+  const numbers = numberSTR.split('');
+
+  let leftTotal = 0;
+  for (let i = 0 ; i < numbers.length / 2 ; i++) {
+    leftTotal += +numbers[i];
+  }
+
+  let rightTotal = 0;
+  for (let i = numbers.length / 2 ; i < numbers.length ; i++) {
+    rightTotal += +numbers[i];
+  }
+
+  return leftTotal === rightTotal ? 'LUCKY' : 'READY';
+}
+
+
+
+
+function ttd() {
+
+  let result = luckyStraight(123402);
+  assert.deepStrictEqual(result, 'LUCKY');
+
+  result = luckyStraight(7755);
+  assert.deepStrictEqual(result, 'READY');
+}
+
+ttd();
