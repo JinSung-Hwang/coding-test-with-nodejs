98f572c70630df1d2b81abd52a91a75e40351444
JinSung-Hwang <wlstjd7782>
2021-07-20 21:48:05 +0900

    feature: 버블 정렬, 선택 정렬 추가

---
+++ sort/bubble-sort.js
@@ -0,0 +1,16 @@
+
+// note: 안정, 제자리 정렬(새로운 메모리 필요x
+
+const bubbleSort = function(array) {
+  let length = array.length;
+  for (let i = 0; i < length - 1; i++) { // note: 횟수
+    for (let j = 0; j < length - 1 - i; j++) { // note: 위치
+      if (array[j] > array[j + 1]) { // note: 두 수를 비교하여 앞 수가 뒷 수보다 크면
+        [array[j], array[j+1]] = [array[j+1], array[j]]
+      }
+    }
+  }
+  return array;
+};
+
+bubbleSort([5,1,7,4,6,3,2,8]);
