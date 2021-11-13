9898f33126678bffabad8a6225a686187fd0cde7
JinSung Hwang <jinsunghwang@HJS.local>
2021-11-13 18:58:24 +0900

    feature: 베이스 코드 작성

---
+++ BaseCode/recursion.js
@@ -0,0 +1,9 @@
+// note: 재귀(recursion)란?
+// note:   자신을 정의할때 자기 자신을 재 참조하는 방법을 뜻한다.
+
+// note: 재귀 사용 이유
+// note:   재귀는 특정 알고리즘의 경우 코드 구현, 가독성을 높여준다. (모든 재귀문은 for, while로 작성가능)
+
+// note: 재귀를 잘하는 법
+// note:   1. 일반적인 절차 지향적 사고에서 '수학적 귀납법'적인 사고를 할 수 있어야한다. (https://www.youtube.com/watch?v=8vDDJm5EewM)
+// note:   2. 노력해보자
--- BaseCode/sort.js
+++ BaseCode/sort.js
@@ -1,7 +1,7 @@
 // note: 정렬(Sorting)이란?
 // note:   특정 조건에 맞춰 원소를 나열하는 방법
 
-// note: 정렬 조건
+// note: 정렬 사용 조건
 // note:   1. 정렬 조건이 필요하다.
 // note:   2. 시간 복잡도는 O(NlogN) 이다.
 // note:     -> O(NlogN) 인데 정렬해도 괜찮은가?
@@ -9,14 +9,15 @@
 // note:     -> in-place(제자리): 정렬하는 과정중에 N개의 만큼 메모리를 추가적으로 사용해도 되는가? 
 // note:     -> stable  (안정적): 동등한 위상의 원소들의 순서가 보존되어야 하는가?
 
+// note: JS 내부 Sort는 TimSort 방식을 사용한다.
+// note:   1. TimSort는 시간 복잡도: O(NlogN), 공간 복잡도: O(N)을 갖는다.
+//     :   2. 팀소트는 내부적으로 insertion, merge sort를 결합한 방식이다
+
 // note: 정렬 특성
 // note:   1. 같은 정보들은 인접해 있을 것이다.
 // note:   2. 각 원소마다, 가장 비슷한 순서의 다른 원소는 자신의 양 옆이다.
 // note:   3. 정렬만 해도 문제가 쉽게 풀리는 경우가 상당히 많다. 
 
-// note: JS 내부 Sort는 TimSort 방식을 사용한다.
-// note:   1. TimSort는 시간 복잡도: O(NlogN), 공간 복잡도: O(N)을 갖는다.
-//     :   2. 팀소트는 내부적으로 insertion, merge sort를 결합한 방식이다
 
 Array.sort((a, b) => {
   return a - b // note: 오름 차순 정렬
