078e8b8cb0cfde7eaa7825bc9323b6c2093652e2
JinSung Hwang <jinsunghwang@HJS.local>
2022-04-23 10:45:28 +0900

    feature: beakjoon input 받는 형식 정리해둠

--- input/backjoon.js
+++ input/backjoon.js
@@ -1,4 +1,3 @@
-
 // note: rl 방식 (terminal에서 표준 입력(stdin), 표준 출력(stdout), 표준 에러(stderr)을 이용하는 방식)
 // 출처: https://ko.wikipedia.org/wiki/%ED%91%9C%EC%A4%80_%EC%8A%A4%ED%8A%B8%EB%A6%BC
 
@@ -9,17 +8,24 @@ const rl = readLine.createInterface({
   output: process.stdout,
 });
 
+let inputs = [];
 rl.on('line', function(line) { // 한줄 입력 받기 
   solution();
   rl.close();
 })
 
-let input = [];
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 rl.on('line', function (line) { // 여러줄 입력 받기
-  input.push(line)
+  inputs.push(line)
 })
   .on('close', function () {
+  input();
   solution();
+  output();
   process.exit();
 });
 
