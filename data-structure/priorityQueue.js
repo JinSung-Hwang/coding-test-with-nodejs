d5e17c48064591213f5986d99d0b1d4699524a28
JinSung Hwang <jinsunghwang@pool-100-10-10-76.prvdri.fios.verizon.net>
2022-10-21 16:51:53 +0900

    feature: 우선순위 큐 추가

--- data-structure/priorityQueue.js
+++ data-structure/priorityQueue.js
@@ -86,3 +86,60 @@ class PriorityQueue {
     this._elements[b] = aux;
   }
 }
+
+// note: PriorityQueue 사용하는 샘플
+
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './beakjun-inputs.txt';
+let result;
+let pQueue;
+let lectureSchedules;
+
+process.platform === 'linux' ? main() : ''; // 로컬 테스트 케이스와 백준 사이트에 호환을 맞춤기 위함
+
+function input() {
+  // * 글로벌 변수 초기화 *
+  result = 0;
+  pQueue = new PriorityQueue((a, b) => {
+    if (b.end === a.end) return (b.end - b.begin) - (a.end - a.begin); // 강의가 시작하는 시간이 같으면 더 짧은것을 우선순위를 높인다.
+    return b.end - a.end;
+  });
+  lectureSchedules = [];
+  // * 데이터 입력 받기 *
+  let inputs = fs.readFileSync(filePath).toString().trim().split('\n');
+  let lectureCount = +inputs.shift();
+  for (let i = 0; i < lectureCount; i++) {
+    let [begin, end] = inputs[i].split(' ');
+    lectureSchedules.push({begin: +begin, end: +end});
+  }
+  lectureSchedules.sort((a, b) => {
+    if (b.begin === a.begin) return (b.end - b.begin) - (a.end - a.begin); // 강의가 시작하는 시간이 같으면 더 짧은것을 우선순위를 높인다.
+    return b.begin - a.begin;
+  });
+}
+
+function output() {
+  console.log(result);
+  return result;
+}
+
+function main() {
+  input();
+  solution();
+  return output();
+}
+
+function solution() {
+  let lectureSchedule = lectureSchedules.pop();
+  pQueue.enq(lectureSchedule);
+  while (lectureSchedules.length) {
+    let lectureSchedule = lectureSchedules.pop();
+    if (lectureSchedule.begin >= pQueue.peek().end) {
+      pQueue.deq();
+      pQueue.enq(lectureSchedule);
+    } else {
+      pQueue.enq(lectureSchedule);
+    }
+  }
+  result = pQueue.size();
+}
